c4dccd55780fab75a6bdc8f5c0180f643814257f
Andrej1999 <malahow.andrej2014@yandex.ru>
2022-06-28 23:45:01 +0700

    Hello

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ b.addEventListener("click", function () {
     a.style.display= "none";
     c.style.display = "block";
 })
+
+const form = document.getElementById('form')
+form.addEventListener('submit', formSand)
+
+function formSand(e) {
+    e.preventDefault();
+    let formDate = new FormDate(form);
+    let respoin = await fetch('index.php', {
+        method:"POST",
+        body:formDate
+    });
+}
